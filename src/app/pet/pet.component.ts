1fb1c86ec503672a54fc1112b20dee333ee78957
Simar Singh <simarpaulsingh@gmail.com>
2018-03-26 01:36:00 -0400

    adding actions to pet data table, view and remove

--- src/app/pet/pet.component.ts
+++ src/app/pet/pet.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Pet, PetService} from "../providers/pet.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-pet',
@@ -10,12 +11,24 @@ export class PetComponent implements OnInit {
 
   list: Pet[];
 
-  displayedColumns = ['id', 'name', 'status', 'category'];
+  displayedColumns = ['id', 'name', 'status', 'category', 'action'];
 
-  constructor(private petService : PetService) { }
+  constructor(private petService : PetService, private router: Router) { }
+
+  details(element: Pet) {
+    this.router.navigate(["/pet",element.id])
+  }
+
+
+  remove(element: Pet) {
+    console.log(element);
+    console.log("Remove")
+  }
+
 
   ngOnInit() {
-    this.petService.getList().subscribe(
+    this.petService.getList()
+      .subscribe(
       {
         next: list => this.list = list
       }
